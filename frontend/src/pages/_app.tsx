65d32446258787aea42a311aa873cd6706650665
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-11-12 13:48:53 +0100

    remove frontend/public/right-vector.svg frontend/public/left-vector.svg .DS_STORE

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,10 +2,6 @@ import "./globals.css";
 import React from 'react';
 import { AppProps } from 'next/app';
 
-// TODO: setup apollo client
-// TODO: setup moution
-// TODO: load poppins hhhhhh font
-
 const Matcha = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };
