74839b720f252c967e7cb4588511e6dd0c402f8f
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-11-12 13:40:59 +0100

    setup

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+import React from 'react';
+import { AppProps } from 'next/app';
+
+// TODO: setup apollo client
+// TODO: setup moution
+// TODO: load poppins hhhhhh font
+
+const Matcha = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default Matcha;
---
+++ frontend/src/pages/auth/@signin/index.tsx
@@ -0,0 +1,1 @@
+// TODO: render sign in form
