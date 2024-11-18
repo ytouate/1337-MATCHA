8bb543542e1f14152a27bed9b7ab7fc7a7f9cdc5
Youssef Touate <touateyoussef2003@gmail.com>
2024-11-18 14:55:15 +0100

    dockerize project and connect to pg with psycopg3

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+import dotenv
+from pathlib import Path
+from .database import database
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv.load_dotenv(BASE_DIR / ".env")
 
 app = FastAPI()
 
 
 @app.get("/")
 async def main_route():
+    with database.PgDatabase() as db:
+        print(db)
     return {"message": "Hey, It is me Goku"}
