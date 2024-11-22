bf2f1fcfa510aa2ecd3bade5cab0ce5d180a1b73
Taha Chtaibi <tehsusrhist@gmail.com>
2024-11-22 18:42:13 +0100

    background task email sender

--- api/app/main.py
+++ api/app/main.py
@@ -6,6 +6,8 @@ from .helpers.utils import get_table_data
 dotenv.load_dotenv()
 
 users_table_data = get_table_data(file_name="user.json")
+# create_table('users', users_table_data)
+
 app = FastAPI()
 
 app.include_router(router)
