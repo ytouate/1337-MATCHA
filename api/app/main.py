ffc566eabd4e3c2d7baa9152e6e25c840a7b92c6
Youssef Touate <touateyoussef2003@gmail.com>
2024-11-28 19:48:47 +0100

    adding cors headers middleware

--- api/app/main.py
+++ api/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 import dotenv
 from .routers.auth import router
 from .helpers.utils import get_table_data
+from fastapi.middleware.cors import CORSMiddleware
+
 
 dotenv.load_dotenv()
 
@@ -9,5 +11,18 @@ users_table_data = get_table_data(file_name="user.json")
 # create_table('users', users_table_data)
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
